0047949a45bd94cda6e93f421c407141ff5e198c
Matheus Lopes <matheus.lopesmdev@gmail.com>
2024-01-21 17:21:36 -0300

    aumento no tempo da solicitacao

--- ExclusaoMutua/exclusaoMutua.py
+++ ExclusaoMutua/exclusaoMutua.py
@@ -3,6 +3,7 @@ import threading
 import queue
 import time
 
+
 class Coordenador:
     def __init__(self):
         self.lider = None
@@ -12,7 +13,9 @@ class Coordenador:
     def inicia_coordenador(self):
         # Inicia o coordenador em uma porta disponível
         self.lider = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.lider.bind(('0.0.0.0', 0))  # 0 indica que a porta será atribuída automaticamente
+        self.lider.bind(
+            ("0.0.0.0", 0)
+        )  # 0 indica que a porta será atribuída automaticamente
         endereco = self.lider.getsockname()
         print(f"Coordenador iniciado na porta {endereco[1]}")
 
@@ -21,13 +24,17 @@ class Coordenador:
         while True:
             # Aguarda uma conexão de um processo
             cliente, endereco_cliente = self.lider.accept()
-            threading.Thread(target=self.lida_com_requisicao, args=(cliente, endereco_cliente)).start()
+            threading.Thread(
+                target=self.lida_com_requisicao, args=(cliente, endereco_cliente)
+            ).start()
 
     def lida_com_requisicao(self, cliente, endereco_cliente):
         with self.mutex:
             # Verifica se há outros processos acessando o recurso
             if not self.fila_de_requisicoes.empty():
-                print(f"Recurso ocupado. Adicionando à fila. Solicitação de {endereco_cliente}.")
+                print(
+                    f"Recurso ocupado. Adicionando à fila. Solicitação de {endereco_cliente}."
+                )
                 self.fila_de_requisicoes.put((cliente, endereco_cliente))
             else:
                 print(f"Recurso livre. Concedendo acesso a {endereco_cliente}.")
@@ -35,7 +42,7 @@ class Coordenador:
 
     def concede_acesso(self, cliente, endereco_cliente):
         # Simula o acesso ao recurso compartilhado com um tempo mais realista
-        tempo_processamento = 5  # 5 segundos de processamento
+        tempo_processamento = 15  # 5 segundos de processamento
         print(f"Iniciando acesso ao recurso por {tempo_processamento} segundos.")
         time.sleep(tempo_processamento)
         print("Acesso concluído.")
@@ -49,6 +56,7 @@ class Coordenador:
         else:
             cliente.sendall(b"Acesso concedido.")
             cliente.close()
+
 
 # Inicia o coordenador
 coordenador = Coordenador()
