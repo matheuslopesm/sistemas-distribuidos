0c28f3df193db0fd8be3af84d0d4b72eafbe1bb9
Matheus Lopes <matheus.lopesmdev@gmail.com>
2024-02-07 21:16:17 -0300

    fix 234234

--- TP02/test.js
+++ TP02/test.js
@@ -1,11 +1,30 @@
-const { exec } = require('child_process');
+const devicesIds = [];
+const devices = [];
 
-let hostname = 'localhost';
-exec("ip route | awk '/default/ {print $3}'", (error, stdout) => {
-    if (error) {
-        console.error('cannot get outer ip address for wrapper service', error);
-    } else {
-        hostname = stdout.replace(/\n/, '');
-        console.log(hostname)
+async function ping(ip) {
+    try {
+        const response = await fetch(`http://${ip}`, { method: 'HEAD' });
+        if (response.ok) {
+            return true; // IP está acessível
+        } else {
+            return false; // Falha ao acessar o IP
+        }
+    } catch (error) {
+        return false; // Erro ao tentar acessar o IP
     }
-});
+}
+
+async function checkDevicesAvailability() {
+    for (let ip_machine = 1; ip_machine <= 10; ip_machine++) {
+        const ip = `172.16.100.${ip_machine}`;
+        const isReachable = await ping(ip);
+        if (isReachable) {
+            devicesIds.push(ip_machine);
+            devices.push({ "ID": ip_machine, "IP": ip });
+        }
+    }
+
+    console.log("Dispositivos acessíveis:", devices);
+}
+
+checkDevicesAvailability();
