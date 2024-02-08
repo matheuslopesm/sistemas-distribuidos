0127879d10dd9d18b94ec7417b62d7d35cf3bfbf
Matheus Lopes <matheus.lopesmdev@gmail.com>
2024-02-07 21:42:27 -0300

    new ping

--- TP02/test.js
+++ TP02/test.js
@@ -1,30 +1,21 @@
+const ping = require('ping');
+
 const devicesIds = [];
 const devices = [];
 
-async function ping(ip) {
-    try {
-        const response = await fetch(`http://${ip}`, { method: 'HEAD' });
-        if (response.ok) {
-            return true; // IP está acessível
-        } else {
-            return false; // Falha ao acessar o IP
-        }
-    } catch (error) {
-        return false; // Erro ao tentar acessar o IP
-    }
+for (let ip_machine = 1; ip_machine < 10; ip_machine++) {
+    const ip = `172.16.100.${ip_machine}`;
+
+    ping.promise.probe(ip)
+        .then((res) => {
+            if (res.alive) {
+                devicesIds.push(ip_machine);
+                devices.push({ "ID": ip_machine, "IP": ip });
+            }
+        })
+        .catch((err) => {
+            console.error('Error occurred while pinging:', err);
+        });
+
+    console.log(devices)
 }
-
-async function checkDevicesAvailability() {
-    for (let ip_machine = 3; ip_machine <= 10; ip_machine++) {
-        const ip = `172.16.100.3`;
-        const isReachable = await ping(ip);
-        if (isReachable) {
-            devicesIds.push(ip_machine);
-            devices.push({ "ID": ip_machine, "IP": ip });
-        }
-    }
-
-    console.log("Dispositivos acessíveis:", devices);
-}
-
-checkDevicesAvailability();
